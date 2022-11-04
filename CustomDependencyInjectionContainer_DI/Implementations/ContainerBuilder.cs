84db3e954f1ceb6a78f658dfc380f54f53b92aee
Nikita <nik.lizard.mobile@gmail.com>
2022-11-04 19:43:06 +0400

    Add ContainerBuilder logic

---
+++ CustomDependencyInjectionContainer_DI/Implementations/ContainerBuilder.cs
@@ -0,0 +1,16 @@
+﻿using CustomDependencyInjectionContainer_DI.Abstractions;
+
+namespace CustomDependencyInjectionContainer_DI.Implementations;
+public class ContainerBuilder : IContainerBuilder
+{
+    private readonly List<ServiceDescriptor> descriptors = new();
+    public void Add(ServiceDescriptor descriptor)
+    {
+        descriptors.Add(descriptor);
+    }
+
+    public IContainer Build()
+    {
+        return new Container(descriptors);
+    }
+}

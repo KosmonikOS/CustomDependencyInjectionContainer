5724c1b60db62eedde12fb25046a57db0f301bc0
Nikita <nik.lizard.mobile@gmail.com>
2023-01-06 18:13:44 +0400

    Add playground

--- CustomDependencyInjectionContainer/Program.cs
+++ CustomDependencyInjectionContainer/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CustomDependencyInjectionContainer_DI.Extentions;
+using CustomDependencyInjectionContainer_DI.Implementations;
+
+var builder = new ContainerBuilder();
+builder.AddSingleton(typeof(IService<>), typeof(Service1<>));
+var container = builder.Build();
+using (container)
+{
+    using (var scope1 = container.CreateScope())
+    {
+        var service1 = scope1.Resolve(typeof(IService<int>));
+    }
+}
+
+public interface IService<T>
+{
+    public void Do();
+}
+public class Service1<T> : IService<T>
+{
+    public void Do()
+    {
+        Console.WriteLine();
+    }
+}
